08983fc43693e9cfeb4a8f4fc8f73a11c341a5bf
Saeed Gholami <saeed.ghollami@gmail.com>
2022-05-29 08:32:55 +0430

    Hambergur menu overlay animation

--- easybank-landing-page-master/easybank-landing-page-master/app/js/script.js
+++ easybank-landing-page-master/easybank-landing-page-master/app/js/script.js
@@ -1,1 +1,17 @@
-console.log("hello there!");
+const btnHamburger = document.querySelector("#btnHamburger");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay"); 
+
+btnHamburger.addEventListener("click", () => {
+    console.log("click hamburger");
+    if(header.classList.contains("open")) {
+        header.classList.remove("open");
+        overlay.classList.remove("fade-in");
+        overlay.classList.add("fade-out");
+    }
+    else {
+        header.classList.add("open");
+        overlay.classList.remove("fade-out");
+        overlay.classList.add("fade-in");
+    }
+});
