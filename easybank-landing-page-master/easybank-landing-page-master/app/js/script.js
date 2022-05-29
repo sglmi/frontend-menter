e8b9000bfef56386dda097f1cd1dd5e0e151095b
Saeed Gholami <saeed.ghollami@gmail.com>
2022-05-29 09:08:26 +0430

    Make hamburger menu

--- easybank-landing-page-master/easybank-landing-page-master/app/js/script.js
+++ easybank-landing-page-master/easybank-landing-page-master/app/js/script.js
@@ -1,17 +1,21 @@
 const btnHamburger = document.querySelector("#btnHamburger");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay"); 
-
+const fadeElems = document.querySelectorAll(".has-fade");
 btnHamburger.addEventListener("click", () => {
     console.log("click hamburger");
     if(header.classList.contains("open")) {
         header.classList.remove("open");
-        overlay.classList.remove("fade-in");
-        overlay.classList.add("fade-out");
+        fadeElems.forEach(element => {
+            element.classList.remove("fade-in");
+            element.classList.add("fade-out");
+        });
     }
     else {
         header.classList.add("open");
-        overlay.classList.remove("fade-out");
-        overlay.classList.add("fade-in");
+        fadeElems.forEach(element => {
+            element.classList.remove("fade-out");
+            element.classList.add("fade-in");
+        });
     }
 });
